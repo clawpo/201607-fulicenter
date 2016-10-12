6e35f455e754af5ba14eb35b07bd17a6a135518c
耿小泰 <clawpo@foxmail.com>
2016-10-12 21:14:12 +0800

    添加分类首页adapter

--- app/src/main/java/cn/ucai/fulicenter/I.java
+++ app/src/main/java/cn/ucai/fulicenter/I.java
@@ -258,7 +258,7 @@ public interface I {
     String UTF_8 = "utf-8";
     String PAGE_ID = "page_id";//分页的起始下标
     String PAGE_SIZE = "page_size";//分页的每页数量
-    int PAGE_ID_DEFAULT = 0;//分页的起始下标默认值
+    int PAGE_ID_DEFAULT = 1;//分页的起始下标默认值
     int PAGE_SIZE_DEFAULT = 10;//分页的每页数量默认值
 
     int ID_DEFAULT=0;//ID默认值
